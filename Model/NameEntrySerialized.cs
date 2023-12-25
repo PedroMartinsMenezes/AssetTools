115d648ad308ad367e71071e3b9e10444a977f2b
pmmenezes <protopedro@gmail.com>
2023-12-25 16:11:24 -0300

    FNameEntrySerialized

---
+++ Model/NameEntrySerialized.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Text;
+
+namespace AssetTool.Model
+{
+    public class FNameEntrySerialized
+    {
+        [Description("New")] public int NameSize => Name.Length > 0 ? Name.Length + 1 : 0;
+
+        public string Name = string.Empty;
+
+        public byte[] AnsiName => Encoding.ASCII.GetBytes(Name).Concat(new byte[2048 - Name.Length]).ToArray();
+
+        public byte[] WideName => Encoding.Unicode.GetBytes(Name).Concat(new byte[2048 - Name.Length * 2]).ToArray();
+    }
+}
