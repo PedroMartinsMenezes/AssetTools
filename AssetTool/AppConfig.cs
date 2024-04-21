216515f9329824c9b8a3edf6ec203e5b5793264d
pmmenezes <protopedro@gmail.com>
2024-04-21 17:50:25 -0300

    Tests OK

--- AssetTool/AppConfig.cs
+++ AssetTool/AppConfig.cs
@@ -2,12 +2,12 @@
 {
     public static class AppConfig
     {
-        public static bool AutoCheck { get; set; } = true;
+        public static bool AutoCheck { get; set; } = false;
 
         public static bool EnableVerboseLog { get; set; } = false;
 
         public static long LogStartOffset { get; set; } = 0;
 
-        public static long LogEndOffset { get; set; } = long.MaxValue;
+        public static long LogEndOffset { get; set; } = 0;
     }
 }
