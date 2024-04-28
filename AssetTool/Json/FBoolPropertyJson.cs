bcd898f9305519c95609f1c47f7034927710bf39
pmmenezes <protopedro@gmail.com>
2024-04-28 19:31:14 -0300

    FBoolPropertyJson OK

--- AssetTool/Json/FBoolPropertyJson.cs
+++ AssetTool/Json/FBoolPropertyJson.cs
@@ -25,41 +25,20 @@ namespace AssetTool
         public static FPropertyTag GetNative(string key, bool value)
         {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
-            var match = Regex.Match(key, FFloatPropertyJson.Pattern);
+            var match = Regex.Match(key, FBoolPropertyJson.Pattern);
             string name = match.Groups[1].Value;
             string index = match.Groups[2].Value;
             string guid = match.Groups[3].Value;
             return new FPropertyTag
             {
                 Name = new FName(name),
-                Type = new FName(FDoubleProperty.TYPE_NAME),
-                Value = value,
-                Size = 8,
+                Type = new FName(FBoolProperty.TYPE_NAME),
+                BoolVal = (byte)(value ? 1 : 0),
+                Size = 0,
                 ArrayIndex = index.Length > 0 ? int.Parse(index) : 0,
                 HasPropertyGuid = (byte)(guid.Length > 0 ? 1 : 0),
                 PropertyGuid = guid.Length > 0 ? new FGuid(guid) : null,
             };
         }
-
-        //public FBoolPropertyJson() { }
-
-        //public FBoolPropertyJson(FPropertyTag tag)
-        //{
-        //    Add($"bool {tag.Name.Value}", tag.BoolVal == 1);
-        //}
-
-        //public FPropertyTag GetNative()
-        //{
-        //    string[] typeName = Keys.First().Split(' ');
-        //    string name = typeName[1];
-        //    bool value = bool.Parse(Values.First().ToString());
-
-        //    return new FPropertyTag { Name = new FName(name), Type = new FName(FBoolProperty.TYPE_NAME), BoolVal = (byte)(value ? 1 : 0) };
-        //}
-
-        //public static FPropertyTag GetNative(string[] v)
-        //{
-        //    return new FPropertyTag { Name = new FName(v[1]), Type = new FName(FBoolProperty.TYPE_NAME), BoolVal = bool.Parse(v[2]) ? (byte)1 : (byte)0 };
-        //}
     }
 }
