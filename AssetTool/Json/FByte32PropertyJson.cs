053d9d7e9f69d2621b1f2424e748750f527e4a94
pmmenezes <protopedro@gmail.com>
2024-04-28 19:31:24 -0300

    FByte32PropertyJson OK

--- AssetTool/Json/FByte32PropertyJson.cs
+++ AssetTool/Json/FByte32PropertyJson.cs
@@ -5,7 +5,7 @@ namespace AssetTool
 {
     public class FByte32PropertyJson : Dictionary<string, object>, IPropertytag
     {
-        public const string Pattern = "byte32 (?:(\\w+))?\\s*'([ \\w]+)'\\s*(?:\\(([-A-F0-9]+)\\))?";
+        public const string Pattern = "byte32 '([ \\w]+)'\\s*(?:\\[(\\d+)\\])?\\s*(?:\\(([-a-fA-F0-9]+)\\))?";
 
         public FByte32PropertyJson() { }
 
