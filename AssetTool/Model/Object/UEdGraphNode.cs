eabe415eaf1859cb75ccfc8b9b0f11d876ea0876
pmmenezes <protopedro@gmail.com>
2024-05-16 00:57:24 -0300

    Move: UEdGraphNode

--- AssetTool/Model/Object/UEdGraphNode.cs
+++ AssetTool/Model/Object/UEdGraphNode.cs
@@ -10,28 +10,11 @@ namespace AssetTool
         public new UEdGraphNode Move(Transfer transfer)
         {
             base.Move(transfer);
-            if (transfer.IsReading)
-                return Read(transfer.reader);
-            else
-                return Write(transfer.writer);
-        }
 
-        private UEdGraphNode Read(BinaryReader reader)
-        {
             if (GlobalObjects.CustomVer(FBlueprintsObjectVersion.Guid) >= (int)FBlueprintsObjectVersion.Enums.EdGraphPinOptimized)
             {
-                Pins = [];
-                Pins.Resize(reader.ReadInt32());
-                Pins.ForEach(x => x.MovePart1(GlobalObjects.Transfer, UEdGraphPin.EPinResolveType.OwningNode));
-            }
-            return this;
-        }
-
-        private UEdGraphNode Write(BinaryWriter writer)
-        {
-            if (GlobalObjects.CustomVer(FBlueprintsObjectVersion.Guid) >= (int)FBlueprintsObjectVersion.Enums.EdGraphPinOptimized)
-            {
-                writer.Write(Pins.Count);
+                Pins ??= [];
+                Pins.Resize(transfer);
                 Pins.ForEach(x => x.MovePart1(GlobalObjects.Transfer, UEdGraphPin.EPinResolveType.OwningNode));
             }
             return this;
