5ce6e596200b004466010e0b47e17fc322940641
pmmenezes <protopedro@gmail.com>
2024-06-09 21:37:04 -0300

    MM_Run_Fwd - OK

--- AssetTool.Test/DataTest/AnimationTest.cs
+++ AssetTool.Test/DataTest/AnimationTest.cs
@@ -6,10 +6,9 @@ namespace AssetTool.Test
         public AnimationTest() => GlobalNames.Clear();
 
         [Fact] public void BS_MM_WalkRun() => Assert.True(StructWriter.RebuildAsset("BS_MM_WalkRun"));
-
         [Fact] public void MM_T_Pose() => Assert.True(StructWriter.RebuildAsset("MM_T_Pose"));
-
         [Fact] public void MM_Jump() => Assert.True(StructWriter.RebuildAsset("MM_Jump"));
+        [Fact] public void MM_Run_Fwd() => Assert.True(StructWriter.RebuildAsset("MM_Run_Fwd"));
 
         //[Fact] public void ABP_Manny() => Assert.True(StructWriter.RebuildAssetInMemory("ABP_Manny"));
 
--- AssetTool/Program.cs
+++ AssetTool/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AssetTool;
 
-StructWriter.RebuildAsset("MM_Jump");
+StructWriter.RebuildAsset("MM_Run_Fwd");
