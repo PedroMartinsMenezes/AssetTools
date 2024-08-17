88784b7fb89413a4f5a64f0542898a875b998385
pmmenezes <protopedro@gmail.com>
2024-08-17 19:11:51 -0300

    CropoutTests

---
+++ AssetTool.Test/DataTest/CropoutTests.cs
@@ -0,0 +1,10 @@
+namespace AssetTool.Test
+{
+    [Collection("Sequential")]
+    public class CropoutTests : TestBase
+    {
+        public CropoutTests() => GlobalNames.Clear();
+
+        [Fact] public void ST_Job() => Assert.True(StructWriter.RebuildAsset("CropoutSampleProject/Villagers/ST_Job.uasset"));
+    }
+}
