f234cb571c240833a191620ce3e32b121be35325
pmmenezes <protopedro@gmail.com>
2024-09-16 10:47:00 -0300

    InputAssets.txt

--- AssetTool/Program.cs
+++ AssetTool/Program.cs
@@ -1,5 +1,12 @@
-﻿using AssetTool;
+﻿namespace AssetTool
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success = StructWriter.RebuildAsset("CropoutSampleProject/Villagers/BPI_Villager");
 
-bool success = StructWriter.RebuildAsset("CropoutSampleProject/Villagers/BPI_Villager");
-
-Console.WriteLine(success ? "\nSUCCESS\n" : "\nFAIL\n");
+            Console.WriteLine(success ? "\nSUCCESS\n" : "\nFAIL\n");
+        }
+    }
+}
