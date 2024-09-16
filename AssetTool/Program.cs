ba11d8f6faf8c1f2d1bf2b71ca0cdbabc5abd64c
pmmenezes <protopedro@gmail.com>
2024-09-16 10:29:09 -0300

    BPI_Villager OK

--- AssetTool/Program.cs
+++ AssetTool/Program.cs
@@ -1,5 +1,5 @@
 ﻿using AssetTool;
 
-bool success = StructWriter.RebuildAsset("CropoutSampleProject/Villagers/BP_Villager");
+bool success = StructWriter.RebuildAsset("CropoutSampleProject/Villagers/BPI_Villager");
 
 Console.WriteLine(success ? "\nSUCCESS\n" : "\nFAIL\n");
