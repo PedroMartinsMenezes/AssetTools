f6f042845938e902831df1831aa2b25852e437b0
pmmenezes <protopedro@gmail.com>
2024-09-21 00:49:22 -0300

    StandardMacros - OK

---
+++ AssetTool/UE/Runtime/CoreUObject/UObject/UObjectRedirector.cs
@@ -0,0 +1,15 @@
+﻿namespace AssetTool
+{
+    [JsonAsset("ObjectRedirector")]
+    public class UObjectRedirector : UObject
+    {
+        public UInt32 DestinationObject;
+
+        public override UObject Move(Transfer transfer)
+        {
+            base.Move(transfer);
+            transfer.Move(ref DestinationObject);
+            return this;
+        }
+    }
+}
