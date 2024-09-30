973195bbfe2b1ecce701648629bdeaf858bb58b6
pmmenezes <protopedro@gmail.com>
2024-09-29 23:11:12 -0300

    DefaultSkeletalMesh - OK

---
+++ AssetTool/UE/Runtime/Engine/Classes/Engine/SkeletalMeshEditorData.cs
@@ -0,0 +1,15 @@
+﻿namespace AssetTool
+{
+    [JsonAsset("SkeletalMeshEditorData")]
+    public class USkeletalMeshEditorData : UObject
+    {
+        public List<FRawSkeletalMeshBulkData> RawSkeletalMeshBulkDatas;
+
+        public override UObject Move(Transfer transfer)
+        {
+            base.Move(transfer);
+            transfer.Move(ref RawSkeletalMeshBulkDatas);
+            return this;
+        }
+    }
+}
