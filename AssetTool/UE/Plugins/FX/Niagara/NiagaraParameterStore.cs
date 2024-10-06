d50f3a0db925d3f8099a4dcb43464a9bb6ffe2a6
pmmenezes <protopedro@gmail.com>
2024-10-06 20:09:49 -0300

    BP_Bot - OK

---
+++ AssetTool/UE/Plugins/FX/Niagara/NiagaraParameterStore.cs
@@ -0,0 +1,19 @@
+﻿namespace AssetTool.UE.Plugins.FX.Niagara
+{
+    [TransferibleStruct("NiagaraVariableWithOffset")]
+    public class FNiagaraVariableWithOffset : FNiagaraVariableBase
+    {
+        public Int32 Offset;
+
+        [Location("bool FNiagaraVariableWithOffset::Serialize(FArchive& Ar)")]
+        public override ITransferible Move(Transfer transfer)
+        {
+            base.Move(transfer);
+            if (Supports.VariablesUseTypeDefRegistry)
+            {
+                transfer.Move(ref Offset);
+            }
+            return this;
+        }
+    }
+}
