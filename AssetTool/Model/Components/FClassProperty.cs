9fc662db50cbbc278575ad1564490f58281945fc
pmmenezes <protopedro@gmail.com>
2024-10-09 19:44:09 -0300

    Fix FClassProperty

--- AssetTool/Model/Components/FClassProperty.cs
+++ AssetTool/Model/Components/FClassProperty.cs
@@ -4,5 +4,14 @@
     {
         public new const string TYPE_NAME = "ClassProperty";
         public override string TypeName => TYPE_NAME;
+
+        public UInt32 MetaClass;
+
+        public override FField Move(Transfer transfer)
+        {
+            base.Move(transfer);
+            transfer.Move(ref MetaClass);
+            return this;
+        }
     }
 }
