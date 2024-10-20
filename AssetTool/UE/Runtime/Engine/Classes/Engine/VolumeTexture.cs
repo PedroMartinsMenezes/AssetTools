b13cabb4da145cd91fdb80e3cb76e9eeb0f24d8d
pmmenezes <protopedro@gmail.com>
2024-10-20 13:02:05 -0300

    VolumeToRender.uasset - OK

---
+++ AssetTool/UE/Runtime/Engine/Classes/Engine/VolumeTexture.cs
@@ -0,0 +1,28 @@
+﻿namespace AssetTool
+{
+    [JsonAsset("VolumeTexture")]
+    public class UVolumeTexture : UTexture
+    {
+        public new FStripDataFlags StripFlags;
+        public FBool bCooked;
+
+        [Location("void UVolumeTexture::Serialize(FArchive& Ar)")]
+        public override UObject Move(Transfer transfer)
+        {
+            base.Move(transfer);
+            transfer.Move(ref StripFlags);
+            transfer.Move(ref bCooked);
+            if (bCooked ?? false)
+            {
+                SerializeCookedPlatformData(transfer);
+            }
+            return this;
+        }
+
+        [Location("void UTexture::SerializeCookedPlatformData(FArchive& Ar, const bool bSerializeMipData)")]
+        private void SerializeCookedPlatformData(Transfer transfer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
