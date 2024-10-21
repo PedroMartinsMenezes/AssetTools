3fa5662b928bc38e6ab086d0b4958ab807e329b5
pmmenezes <protopedro@gmail.com>
2024-10-20 23:31:31 -0300

    T_SmokeSubUV_8X8_SubUV.uasset - OK

---
+++ AssetTool/UE/Runtime/Engine/Classes/Particles/SubUVAnimation.cs
@@ -0,0 +1,31 @@
+﻿namespace AssetTool
+{
+    [JsonAsset("SubUVAnimation")]
+    public class USubUVAnimation : UObject
+    {
+        public FBool bCooked;
+        public FSubUVDerivedData DerivedData;
+
+        public override UObject Move(Transfer transfer)
+        {
+            base.Move(transfer);
+            transfer.Move(ref bCooked);
+            if (bCooked ?? false)
+            {
+                transfer.Move(ref DerivedData);
+            }
+            return this;
+        }
+    }
+
+    public class FSubUVDerivedData : ITransferible
+    {
+        public List<FVector2f> BoundingGeometry;
+
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref BoundingGeometry);
+            return this;
+        }
+    }
+}
