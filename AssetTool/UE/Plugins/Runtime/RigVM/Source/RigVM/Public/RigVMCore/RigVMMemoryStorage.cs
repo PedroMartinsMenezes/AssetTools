acc3369b9a7c76277d152dd8a22dd47aeecdf7c9
pmmenezes <protopedro@gmail.com>
2024-11-11 00:38:29 -0300

    URigVMMemoryStorageGeneratorClass

--- AssetTool/UE/Plugins/Runtime/RigVM/Source/RigVM/Public/RigVMCore/RigVMMemoryStorage.cs
+++ AssetTool/UE/Plugins/Runtime/RigVM/Source/RigVM/Public/RigVMCore/RigVMMemoryStorage.cs
@@ -3,9 +3,16 @@ namespace AssetTool
     [JsonAsset("RigVMMemoryStorageGeneratorClass")]
     public class URigVMMemoryStorageGeneratorClass : UClass
     {
+        public List<FRigVMPropertyPathDescription> PropertyPathDescriptions;
+        public ERigVMMemoryType MemoryType;
+
+        [Location("void URigVMMemoryStorageGeneratorClass::Serialize(FArchive& Ar)")]
         public override UObject Move(Transfer transfer)
         {
-            return base.Move(transfer);
+            base.Move(transfer);
+            transfer.Move(ref PropertyPathDescriptions);
+            MemoryType = (ERigVMMemoryType)transfer.Move((byte)MemoryType);
+            return this;
         }
     }
 
