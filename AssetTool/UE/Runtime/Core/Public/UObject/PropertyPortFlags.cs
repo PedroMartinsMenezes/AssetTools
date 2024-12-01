04c00e88a1891ebb79b175b6b6358f9b65f5b356
pmmenezes <protopedro@gmail.com>
2024-12-01 20:40:28 -0300

    EPropertyPortFlags

---
+++ AssetTool/UE/Runtime/Core/Public/UObject/PropertyPortFlags.cs
@@ -0,0 +1,35 @@
+﻿namespace AssetTool
+{
+    public enum EPropertyPortFlags
+    {
+        PPF_None = 0x00000000,
+        PPF_Delimited = 0x00000001,
+        PPF_CheckReferences = 0x00000002,
+        PPF_ExportsNotFullyQualified = 0x00000004,
+        PPF_AttemptNonQualifiedSearch = 0x00000008,
+        PPF_RestrictImportTypes = 0x00000010,
+        PPF_SerializedAsImportText = 0x00000020,
+        PPF_ExternalEditor = 0x00000040,
+        PPF_SubobjectsOnly = 0x00000080,
+        PPF_DeepComparison = 0x00000100,
+        PPF_DeepCompareInstances = 0x00000200,
+        PPF_DeepCompareDSOsOnly = 0x00000400,
+        PPF_Copy = 0x00000800,
+        PPF_Duplicate = 0x00001000,
+        PPF_SimpleObjectText = 0x00002000,
+        PPF_ForDiff = 0x00004000,
+        PPF_ParsingDefaultProperties = 0x00008000,
+        PPF_IncludeTransient = 0x00020000,
+        PPF_InstanceSubobjects = 0x00040000,
+        PPF_PropertyWindow = 0x00080000,
+        PPF_DebugDump = 0x00200000,
+        PPF_DuplicateForPIE = 0x00400000,
+        PPF_SeparateDeclare = 0x00800000,
+        PPF_SeparateDefine = 0x01000000,
+        PPF_BlueprintDebugView = 0x02000000,
+        PPF_ConsoleVariable = 0x04000000,
+        PPF_UseDeprecatedProperties = 0x08000000,
+        PPF_ForceTaggedSerialization = 0x20000000,
+        PPF_DuplicateVerbatim = 0x40000000,
+    };
+}
