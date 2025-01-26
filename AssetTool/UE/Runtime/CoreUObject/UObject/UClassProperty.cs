6389dc7b774322d7276d2cf233035a87b45e545d
pmmenezes <protopedro@gmail.com>
2025-01-26 11:11:13 -0300

    ImportedSequencesHelper - 3413

--- AssetTool/UE/Runtime/CoreUObject/UObject/UClassProperty.cs
+++ AssetTool/UE/Runtime/CoreUObject/UObject/UClassProperty.cs
@@ -3,5 +3,14 @@
     [JsonAsset("ClassProperty")]
     public class UClassProperty : UObjectProperty
     {
+        public UInt32 MetaClass;
+
+        [Location("void UClassProperty::Serialize(FArchive& Ar)")]
+        public override UObject Move(Transfer transfer)
+        {
+            base.Move(transfer);
+            transfer.Move(ref MetaClass);
+            return this;
+        }
     }
 }
