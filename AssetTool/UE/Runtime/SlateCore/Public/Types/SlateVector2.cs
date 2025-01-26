afa73fcf6a53d7d52afd9d5a8fc3483cf3eb1173
pmmenezes <protopedro@gmail.com>
2025-01-26 11:53:17 -0300

    CineCameraRigRail_POC_Widget - 3457

---
+++ AssetTool/UE/Runtime/SlateCore/Public/Types/SlateVector2.cs
@@ -0,0 +1,16 @@
+﻿namespace AssetTool.UE.Runtime.SlateCore.Public.Types
+{
+    [TransferibleStruct("DeprecateSlateVector2D")]
+    public class FDeprecateSlateVector2D : ITransferible
+    {
+        public float X;
+        public float Y;
+
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref X);
+            transfer.Move(ref Y);
+            return this;
+        }
+    }
+}
