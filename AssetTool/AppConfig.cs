eb4bad2cc2d1e2bc4d683de3f484e6d01de9011c
pmmenezes <protopedro@gmail.com>
2025-04-14 00:26:17 -0300

    AppConfig

--- AssetTool/AppConfig.cs
+++ AssetTool/AppConfig.cs
@@ -3,7 +3,7 @@
     public static class AppConfig
     {
         /// Check the bytes FPropertyTag after reading its children members
-        public static bool AutoCheck { get; set; } = true;
+        public static bool AutoCheck { get; set; } = false;
 
         /// Check the bytes of every FPropertyTag child
         ///public static bool AutoCheckDeep { get; set; } = false;
@@ -16,9 +16,9 @@
 
         public static bool SaveHeader { get; set; } = false;
 
-        public static bool SaveJson { get; set; } = true;
+        public static bool SaveJson { get; set; } = false;
 
-        public static bool SaveJsonUnitTest { get; set; } = true;
+        public static bool SaveJsonUnitTest { get; set; } = false;
 
         public static long LogStartOffset { get; set; } = 0;
 
