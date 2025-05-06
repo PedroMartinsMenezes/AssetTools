d9380d832542fef100f7fcc9eadb6d3d20f29fcd
pmmenezes <protopedro@gmail.com>
2025-05-05 23:39:07 -0300

    Failed Files: 1188

--- AssetTool/UE/Runtime/Engine/Public/WorldPartition/Filter/WorldPartitionActorFilter.cs
+++ AssetTool/UE/Runtime/Engine/Public/WorldPartition/Filter/WorldPartitionActorFilter.cs
@@ -3,9 +3,46 @@ namespace AssetTool
     [TransferibleStruct("WorldPartitionActorFilter")]
     public class FWorldPartitionActorFilter : ITransferible
     {
+        public uint32 DataLayerFilterCount;
+        public List<FString> AssetPathStr;
+        public List<FSoftObjectPath> AssetPath;
+        public List<FBool> bIncluded;
+        public uint32 ChildFilterCount;
+        public List<FGuid> ActorGuid;
+        public List<FWorldPartitionActorFilter> ChildFilter;
+
+        [Location("bool FWorldPartitionActorFilter::Serialize(FArchive& Ar)")]
         public ITransferible Move(Transfer transfer)
         {
-            throw new NotImplementedException();
+            transfer.Move(ref DataLayerFilterCount);
+            bIncluded = bIncluded.Resize(transfer, DataLayerFilterCount);
+
+            for (int i = 0; i < DataLayerFilterCount; ++i)
+            {
+                if (!transfer.Supports.WorldPartitionActorDescSerializeSoftObjectPathSupport || transfer.Supports.WorldPartitionActorFilterStringAssetPath)
+                {
+                    AssetPathStr ??= AssetPathStr.Resize(transfer, DataLayerFilterCount);
+                    AssetPathStr[i].Move(transfer);
+                }
+                else
+                {
+                    AssetPath ??= AssetPath.Resize(transfer, DataLayerFilterCount);
+                    AssetPath[i].Move(transfer);
+                }
+                bIncluded[i].Move(transfer);
+            }
+
+            transfer.Move(ref ChildFilterCount);
+            ActorGuid = ActorGuid.Resize(transfer, ChildFilterCount);
+            ChildFilter = ChildFilter.Resize(transfer, ChildFilterCount);
+
+            for (int i = 0; i < ChildFilterCount; ++i)
+            {
+                ActorGuid[i].Move(transfer);
+                ChildFilter[i].Move(transfer);
+            }
+
+            return this;
         }
     }
 }
