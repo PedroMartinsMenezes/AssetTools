deb465357619a74bdda89bd5b1c200fda9ed8f26
pmmenezes <protopedro@gmail.com>
2025-05-12 23:45:18 -0300

    Failed files: 696 *

--- AssetTool/UE/Plugins/Experimental/ChaosClothAsset/Source/ChaosClothAssetEngine/Public/ChaosClothAsset/ClothLodTransitionDataCache.cs
+++ AssetTool/UE/Plugins/Experimental/ChaosClothAsset/Source/ChaosClothAssetEngine/Public/ChaosClothAsset/ClothLodTransitionDataCache.cs
@@ -3,9 +3,19 @@ namespace AssetTool
     [TransferibleStruct("ChaosClothAssetLodTransitionDataCache")]
     public class FChaosClothAssetLodTransitionDataCache : ITransferible
     {
+        public UScriptStruct Struct = new();
+        public FMD5Hash ModelHash;
+        public List<FMeshToMeshVertData> LODTransitionUpData;
+        public List<FMeshToMeshVertData> LODTransitionDownData;
+
+        [Location("bool FChaosClothAssetLodTransitionDataCache::Serialize(FArchive& Ar)")]
         public ITransferible Move(Transfer transfer)
         {
-            throw new NotImplementedException();
+            Struct.SerializeTaggedProperties(transfer);
+            transfer.Move(ref ModelHash);
+            transfer.Move(ref LODTransitionUpData);
+            transfer.Move(ref LODTransitionDownData);
+            return this;
         }
     }
 }
--- AssetTool/UE/Runtime/Core/Public/Misc/FMD5Hash.cs
+++ AssetTool/UE/Runtime/Core/Public/Misc/FMD5Hash.cs
@@ -1,8 +1,18 @@
 ﻿namespace AssetTool
 {
-    public class FMD5Hash
+    public class FMD5Hash : ITransferible
     {
-        public bool bIsValid;
+        public FBool bIsValid;
         public byte[] Bytes = new byte[16];
+
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref bIsValid);
+            if (bIsValid)
+            {
+                transfer.Move(ref Bytes, 16);
+            }
+            return this;
+        }
     }
 }
