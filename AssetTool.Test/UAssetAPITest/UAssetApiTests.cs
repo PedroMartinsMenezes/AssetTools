28d5f9ba7987caf518d3af73e0f10abc037d7ec6
Menezes, Pedro <pedro.menezes@dell.com>
2025-05-27 14:23:24 -0300

    Test

---
+++ AssetTool.Test/UAssetAPITest/UAssetApiTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace AssetTool.Test.UAssetAPITest
+{
+    public class UAssetApiTests : TestBase
+    {
+        [Test] public void PB_DT_RandomizerRoomCheck() => Assert.That(StructWriter.RebuildAsset("UAssetAPI/TestManyAssets/Bloodstained/PB_DT_RandomizerRoomCheck.uasset"));
+    }
+}
