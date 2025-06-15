b593a7da46eaa92d9259509b09abeb6f7b0efb42
pmmenezes <protopedro@gmail.com>
2025-06-15 15:29:32 -0300

    AnimatedMannyPawnExtended - OK

---
+++ AssetTool/UE/Plugins/Experimental/Mover/Source/MoverEditor/Public/K2Node_PlayMontageOnMoverActor.cs
@@ -0,0 +1,11 @@
+namespace AssetTool
+{
+    [JsonAsset("K2Node_PlayMontageOnMoverActor")]
+    public class UK2Node_PlayMontageOnMoverActor : UK2Node_BaseAsyncTask
+    {
+        public override UObject Move(Transfer transfer)
+        {
+            return base.Move(transfer);
+        }
+    }
+}
