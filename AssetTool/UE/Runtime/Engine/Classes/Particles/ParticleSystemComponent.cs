2fb6f3a710855d9867fa38a36b516d7ff5a431e1
pmmenezes <protopedro@gmail.com>
2025-07-05 20:49:53 -0300

    UParticleSystemComponent

--- AssetTool/UE/Runtime/Engine/Classes/Particles/ParticleSystemComponent.cs
+++ AssetTool/UE/Runtime/Engine/Classes/Particles/ParticleSystemComponent.cs
@@ -1,6 +1,12 @@
 ﻿namespace AssetTool
 {
+    [JsonAsset("FXSystemComponent")]
     public class UFXSystemComponent : UPrimitiveComponent
+    {
+    }
+
+    [JsonAsset("ParticleSystemComponent")]
+    public class UParticleSystemComponent : UFXSystemComponent
     {
     }
 }
