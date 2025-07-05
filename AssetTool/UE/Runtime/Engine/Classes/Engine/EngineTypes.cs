8830ac327c9c937f695ffad5bc0680c60bdfb085
pmmenezes <protopedro@gmail.com>
2025-07-04 21:45:06 -0300

    FLightmassPrimitiveSettings

--- AssetTool/UE/Runtime/Engine/Classes/Engine/EngineTypes.cs
+++ AssetTool/UE/Runtime/Engine/Classes/Engine/EngineTypes.cs
@@ -8,4 +8,35 @@ namespace AssetTool
             return base.Move(transfer);
         }
     }
+
+    public struct FLightmassPrimitiveSettings : ITransferible
+    {
+        public FBool bUseTwoSidedLighting;
+        public FBool bShadowIndirectOnly;
+        public float FullyOccludedSamplesFraction;
+        public FBool bUseEmissiveForStaticLighting;
+        public FBool bUseVertexNormalForHemisphereGather;
+        public float EmissiveLightFalloffExponent;
+        public float EmissiveLightExplicitInfluenceRadius;
+        public float EmissiveBoost;
+        public float DiffuseBoost;
+
+        [Location("FArchive& operator<<(FArchive& Ar, FLightmassPrimitiveSettings& Settings)")]
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref bUseTwoSidedLighting);
+            transfer.Move(ref bShadowIndirectOnly);
+            transfer.Move(ref FullyOccludedSamplesFraction);
+            transfer.Move(ref bUseEmissiveForStaticLighting);
+            if (transfer.Supports.VER_UE4_NEW_LIGHTMASS_PRIMITIVE_SETTING)
+            {
+                transfer.Move(ref bUseVertexNormalForHemisphereGather);
+            }
+            transfer.Move(ref EmissiveLightFalloffExponent);
+            transfer.Move(ref EmissiveLightExplicitInfluenceRadius);
+            transfer.Move(ref EmissiveBoost);
+            transfer.Move(ref DiffuseBoost);
+            return this;
+        }
+    }
 }
