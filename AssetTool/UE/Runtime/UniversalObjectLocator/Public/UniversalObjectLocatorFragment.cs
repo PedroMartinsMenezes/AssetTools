abed0f956da10a877d8ab3ad0b8a07c4637b4ceb
pmmenezes <protopedro@gmail.com>
2025-07-06 23:51:14 -0300

    Seq_CineCameraActor03 - OK

---
+++ AssetTool/UE/Runtime/UniversalObjectLocator/Public/UniversalObjectLocatorFragment.cs
@@ -0,0 +1,20 @@
+﻿namespace AssetTool
+{
+    [TransferibleStruct("UniversalObjectLocatorFragment")]
+    public class FUniversalObjectLocatorFragment : ITransferible
+    {
+        public FName FragmentTypeID;
+        public UScriptStruct ScriptStruct;
+
+        [Location("bool FUniversalObjectLocatorFragment::Serialize(FArchive& Ar)")]
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref FragmentTypeID);
+            if (FragmentTypeID.IsFilled())
+            {
+                transfer.Move(ref ScriptStruct, x => x.SerializeItem(transfer));
+            }
+            return this;
+        }
+    }
+}
