b013ff93637641166df61aa05d28f21f849d120d
pmmenezes <protopedro@gmail.com>
2025-07-07 23:13:28 -0300

    TP_TopDown

---
+++ AssetTool/UE/Runtime/Engine/Classes/AI/Navigation/NavAgentSelector.cs
@@ -0,0 +1,15 @@
+﻿namespace AssetTool
+{
+    [TransferibleStruct("NavAgentSelector")]
+    public class FNavAgentSelector : ITransferible
+    {
+        public uint32 PackedBits;
+
+        [Location("bool FNavAgentSelector::Serialize(FArchive& Ar)")]
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref PackedBits);
+            return this;
+        }
+    }
+}
