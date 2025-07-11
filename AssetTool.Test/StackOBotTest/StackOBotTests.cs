05b695c42e4362605e2e2988ea38f86c4d644c11
pmmenezes <protopedro@gmail.com>
2025-07-10 22:39:33 -0300

    Test_StackOBot_All - OK

--- AssetTool.Test/StackOBotTest/StackOBotTests.cs
+++ AssetTool.Test/StackOBotTest/StackOBotTests.cs
@@ -1,13 +1,11 @@
 using NUnit.Framework;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 
 namespace AssetTool.Test.StackOBotTest
 {
     public class StackOBotTests : TestBase
     {
-        [Ignore("Fix")]
         [Test]
         public async System.Threading.Tasks.Task Test_StackOBot_All()
         {
@@ -16,49 +14,9 @@ namespace AssetTool.Test.StackOBotTest
             w.Start();
             int i = 0;
             await System.Threading.Tasks.Parallel.ForEachAsync(files, async (file, ct) =>
-            //foreach (string file in files)
             {
                 bool success = await StructWriter.RebuildAssetFastAsync(file, "");
                 Assert.That(success, $"[{i++}] {file}");
-                //}
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
-        }
-
-        [Test]
-        public async System.Threading.Tasks.Task Test_StackOBot_ExternalActors()
-        {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("StackOBot_Files.txt");
-            w.Start();
-            int i = 0;
-            //await System.Threading.Tasks.Parallel.ForEachAsync(files, async (file, ct) =>
-            foreach (string file in files)
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                Assert.That(success, $"[{i++}] {file}");
-            }
-            //});
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
-        }
-
-        [Test]
-        public async System.Threading.Tasks.Task Test_StackOBot_OK()
-        {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("StackOBot_Files.txt").Where(x => !x.Contains("__ExternalActors__")).ToArray();
-            w.Start();
-            int i = 0;
-            await System.Threading.Tasks.Parallel.ForEachAsync(files, async (file, ct) =>
-            //foreach (string file in files)
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                Assert.That(success, $"[{i++}] {file}");
-                //}
             });
             w.Stop();
             TestContext.WriteLine($"File Count   : {files.Length}");
