6adbfa94cc5632e26a2d27337e5e424d008dc8e3
pmmenezes <protopedro@gmail.com>
2025-07-12 14:59:38 -0300

    Perf Project

---
+++ AssetTool.Perf/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace AssetTool
+{
+    public static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cultureInfo = CultureInfo.InvariantCulture;
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+            string path = "StackOBot_Files.txt";
+            Console.WriteLine($"Processing: {path} ...");
+
+            Stopwatch w = new Stopwatch();
+            var files = await File.ReadAllLinesAsync(path);
+            w.Start();
+            int i = 0;
+
+            bool success1 = await StructWriter.RebuildAssetFastAsync(files[0], "");
+            bool success2 = await StructWriter.RebuildAssetFastAsync(files[1], "");
+
+            //CancellationTokenSource cts = new();
+            //await Parallel.ForEachAsync(files, new ParallelOptions { CancellationToken = cts.Token }, async (file, ct) =>
+            //{
+            //    bool success = await StructWriter.RebuildAssetFastAsync(file, "");
+            //    if (!success)
+            //    {
+            //        await cts.CancelAsync();
+            //        return;
+            //    }
+            //});
+            //cts.Dispose();
+            w.Stop();
+            Console.WriteLine($"File Count   : {files.Length}");
+            Console.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+        }
+    }
+}
