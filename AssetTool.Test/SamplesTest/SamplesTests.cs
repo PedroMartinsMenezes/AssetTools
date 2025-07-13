8bb4edd3547ca52535e0e10847fd420030e1adea
pmmenezes <protopedro@gmail.com>
2025-07-13 13:26:40 -0300

    Lyra files

--- AssetTool.Test/SamplesTest/SamplesTests.cs
+++ AssetTool.Test/SamplesTest/SamplesTests.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AssetTool.Test.SamplesTest
@@ -56,6 +58,47 @@ namespace AssetTool.Test.SamplesTest
             w.Stop();
             TestContext.WriteLine($"File Count   : {files.Length}");
             TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+        }
+
+        [Test]
+        public async Task Test_04_Lyra_Assets()
+        {
+            Stopwatch w = new Stopwatch();
+            List<string> LyraSucceeded = [];
+            List<string> LyraFailed = [];
+            var files = File.ReadAllLines("Lyra_Files.txt");
+            w.Start();
+            int i = 0;
+            await Parallel.ForEachAsync(files, async (file, ct) =>
+            {
+                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
+                if (success) LyraSucceeded.Add(file); else LyraFailed.Add(file);
+                //Assert.That(success, $"[{i++}] {file}");
+            });
+            w.Stop();
+            TestContext.WriteLine($"File Count   : {files.Length}");
+            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+            File.WriteAllLines("Lyra_Failed.txt", LyraFailed);
+            File.WriteAllLines("Lyra_Succeeded.txt", LyraSucceeded);
+        }
+
+        [Test]
+        public async Task Test_04_Lyra_Failed()
+        {
+            Stopwatch w = new Stopwatch();
+            var files = File.ReadAllLines("Lyra_Failed.txt");
+            var Lyra_Failed = files.ToList();
+            w.Start();
+            int i = 0;
+            await Parallel.ForEachAsync(files, async (file, ct) =>
+            {
+                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
+                if (success) Lyra_Failed.Remove(file);
+            });
+            w.Stop();
+            TestContext.WriteLine($"File Count   : {files.Length}");
+            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+            File.WriteAllLines("Lyra_Failed.txt", Lyra_Failed);
         }
     }
 }
