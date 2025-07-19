36cba04bc93448d7db856870452b6d9b58fe1036
pmmenezes <protopedro@gmail.com>
2025-07-19 17:53:48 -0300

    GE_Damage_Melee - OK

--- AssetTool/UE/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Public/GameplayEffect.cs
+++ AssetTool/UE/Plugins/Runtime/GameplayAbilities/Source/GameplayAbilities/Public/GameplayEffect.cs
@@ -8,4 +8,25 @@ namespace AssetTool
             return null;
         }
     }
+
+    [TransferibleStruct("GameplayEffectVersion")]
+    public class FGameplayEffectVersion : ITransferible
+    {
+        public EGameplayEffectVersion CurrentVersion;
+
+        [Location("bool FGameplayEffectVersion::Serialize(FStructuredArchive::FSlot Slot)")]
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.MoveEnum(ref CurrentVersion);
+            return this;
+        }
+    }
+
+    public enum EGameplayEffectVersion : uint8
+    {
+        Monolithic,
+        Modular53,
+        AbilitiesComponent53,
+        Current = AbilitiesComponent53
+    }
 }
