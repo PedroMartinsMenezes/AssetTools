62cedfae8405b2c34a0c811fc02062f3f87ec0ad
pmmenezes <protopedro@gmail.com>
2025-08-09 22:36:22 -0300

    iphone12 - OK

--- AssetTool/UE/Plugins/MetaHuman/MetaHumanAnimator/Source/MetaHumanConfig/Public/MetaHumanConfig.cs
+++ AssetTool/UE/Plugins/MetaHuman/MetaHumanAnimator/Source/MetaHumanConfig/Public/MetaHumanConfig.cs
@@ -3,9 +3,38 @@ namespace AssetTool
     [JsonAsset("MetaHumanConfig")]
     public class UMetaHumanConfig : UObject
     {
+        public FByteBulkData SolverTemplateDataCipherText;
+        public FByteBulkData SolverConfigDataCipherText;
+        public FByteBulkData SolverDefinitionsCipherText;
+        public FByteBulkData SolverHierarchicalDefinitionsCipherText;
+        public FByteBulkData SolverPCAFromDNACipherText;
+        public FByteBulkData FittingTemplateDataCipherText;
+        public FByteBulkData FittingConfigDataCipherText;
+        public FByteBulkData FittingConfigTeethDataCipherText;
+        public FByteBulkData FittingIdentityModelDataCipherText;
+        public FByteBulkData FittingControlsDataCipherText;
+        public FByteBulkData PredictiveGlobalTeethTrainingData;
+        public FByteBulkData PredictiveTrainingData;
+
+        [Location("void UMetaHumanConfig::Serialize(FArchive& Ar)")]
         public override ITransferible Move(Transfer transfer)
         {
-            return base.Move(transfer);
+            base.Move(transfer);
+
+            transfer.Move(ref SolverTemplateDataCipherText);
+            transfer.Move(ref SolverConfigDataCipherText);
+            transfer.Move(ref SolverDefinitionsCipherText);
+            transfer.Move(ref SolverHierarchicalDefinitionsCipherText);
+            transfer.Move(ref SolverPCAFromDNACipherText);
+            transfer.Move(ref FittingTemplateDataCipherText);
+            transfer.Move(ref FittingConfigDataCipherText);
+            transfer.Move(ref FittingConfigTeethDataCipherText);
+            transfer.Move(ref FittingIdentityModelDataCipherText);
+            transfer.Move(ref FittingControlsDataCipherText);
+            transfer.Move(ref PredictiveGlobalTeethTrainingData);
+            transfer.Move(ref PredictiveTrainingData);
+
+            return this;
         }
     }
 }
