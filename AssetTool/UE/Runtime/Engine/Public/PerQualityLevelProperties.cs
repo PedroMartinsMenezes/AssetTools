e2d02bceaa042ce9203a0cb1d9a849d71567c3ad
pmmenezes <protopedro@gmail.com>
2025-08-15 20:31:47 -0300

    Face_LODSettings_High - OK

---
+++ AssetTool/UE/Runtime/Engine/Public/PerQualityLevelProperties.cs
@@ -0,0 +1,28 @@
+﻿namespace AssetTool
+{
+    [TransferibleStruct("PerQualityLevelInt")]
+    public class FPerQualityLevelInt : FPerQualityLevelProperty
+    {
+        public ERayTracingPayloadType Default;
+        public Dictionary<TInt32, TInt32> PerQuality;
+
+        public override ITransferible Move(Transfer transfer)
+        {
+            base.Move(transfer);
+            transfer.MoveEnum(ref Default);
+            transfer.Move(ref PerQuality);
+            return this;
+        }
+    }
+
+    public class FPerQualityLevelProperty : ITransferible
+    {
+        public bool bCooked;
+
+        public virtual ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref bCooked);
+            return this;
+        }
+    }
+}
