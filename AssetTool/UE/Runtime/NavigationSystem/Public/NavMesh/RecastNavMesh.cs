3f28825a6597ed0712a479b49e423eded6feffe2
pmmenezes <protopedro@gmail.com>
2025-08-20 01:34:38 -0300

    L_PathfindingMovement.umap

--- AssetTool/UE/Runtime/NavigationSystem/Public/NavMesh/RecastNavMesh.cs
+++ AssetTool/UE/Runtime/NavigationSystem/Public/NavMesh/RecastNavMesh.cs
@@ -3,9 +3,19 @@ namespace AssetTool
     [JsonAsset("RecastNavMesh")]
     public class ARecastNavMesh : ANavigationData
     {
+        public uint32 NavMeshVersion;
+        public uint32 RecastNavMeshSizeBytes;
+        public byte[] Pad;
+
         public override ITransferible Move(Transfer transfer)
         {
-            return base.Move(transfer);
+            base.Move(transfer);
+
+            transfer.Move(ref NavMeshVersion);
+            transfer.Move(ref RecastNavMeshSizeBytes);
+            transfer.Move(ref Pad, (int)(RecastNavMeshSizeBytes - 4));
+
+            return this;
         }
     }
 }
