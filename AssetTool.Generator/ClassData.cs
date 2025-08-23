b7c87d3f222ee283cca386a306d306002a9d36d9
pmmenezes <protopedro@gmail.com>
2025-08-22 23:24:20 -0300

    Fix ClassAttribute

--- AssetTool.Generator/ClassData.cs
+++ AssetTool.Generator/ClassData.cs
@@ -4,6 +4,15 @@
     {
         public string ClassName { get; set; }
         public string BaseClassName { get; set; }
-        public string ClassAttribute => ClassName.Substring(1);
+        public string ClassAttribute
+        {
+            get
+            {
+                if (!ClassName.Contains("DEPRECATED_"))
+                    return ClassName.Substring(1);
+                else
+                    return ClassName.Replace("DEPRECATED_", "").Substring(1);
+            }
+        }
     }
 }
