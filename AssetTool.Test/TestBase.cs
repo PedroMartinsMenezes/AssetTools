4e95675bf67e3a537b3352291b1d7c524a1f9e65
pmmenezes <protopedro@gmail.com>
2025-09-14 23:57:26 -0300

    UE53 Files

--- AssetTool.Test/TestBase.cs
+++ AssetTool.Test/TestBase.cs
@@ -1,7 +1,12 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AssetTool.Test
 {
@@ -25,7 +30,84 @@ namespace AssetTool.Test
         [TearDown]
         public void TearDown()
         {
-            TestContext.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            TestContext.WriteLine($"Test Finished: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
+        }
+
+        protected async Task Test_UE_Files(string name, bool saveFiles = false)
+        {
+            ConcurrentBag<string> failedFiles = new();
+            ConcurrentBag<string> succeededFiles = new();
+            Stopwatch w = new Stopwatch();
+            var files = File.ReadAllLines($"{name}.txt");
+            w.Start();
+            await Parallel.ForEachAsync(files, async (file, ct) =>
+            {
+                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
+                UpdateFailedFiles(success, file, failedFiles, succeededFiles);
+            });
+            w.Stop();
+            if (saveFiles)
+            {
+                SaveFiles(name, files, failedFiles, succeededFiles);
+            }
+            TestContext.WriteLine($"File         : {name}.txt");
+            TestContext.WriteLine($"File Count   : {files.Length}");
+            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+        }
+
+        private void UpdateFailedFiles(bool success, string file, ConcurrentBag<string> failedFiles, ConcurrentBag<string> succeededFiles)
+        {
+            if (!success)
+            {
+                failedFiles.Add(file);
+            }
+            else
+            {
+                succeededFiles.Add(file);
+            }
+        }
+
+        private void SaveFiles(string name, string[] allFiles, ConcurrentBag<string> failedFiles, ConcurrentBag<string> succeededFiles)
+        {
+            HashSet<string> failedHashset = failedFiles.ToHashSet();
+            failedFiles.Clear();
+
+            HashSet<string> succeededHashset = succeededFiles.ToHashSet();
+            succeededFiles.Clear();
+
+            List<string> succeededFilesSorted = [];
+            List<string> failedFilesSorted = [];
+
+            foreach (string file in allFiles)
+            {
+                if (failedHashset.Contains(file))
+                {
+                    failedFilesSorted.Add(file);
+                }
+                else if (succeededHashset.Contains(file))
+                {
+                    succeededFilesSorted.Add(file);
+                }
+            }
+
+            if (failedFilesSorted.Count > 0)
+            {
+                UpdateLaunchSettingts(failedFilesSorted.First(), false);
+            }
+
+            File.WriteAllLines($"{name}_Failed.txt", failedFilesSorted);
+            File.WriteAllLines($"{name}_Succeeded.txt", succeededFilesSorted);
+        }
+
+        private static void UpdateLaunchSettingts(string file, bool success)
+        {
+            if (!success)
+            {
+                string path = "AssetTool\\Properties\\launchSettings.json";
+                string[] lines = File.ReadAllLines(path);
+                lines[5] = $"      \"commandLineArgs\": \"{file.Replace('\\', '/')} -log\"";
+                File.WriteAllLines(path, lines);
+            }
         }
     }
 }
--- AssetTool.Test/UETest/UETests.cs
+++ AssetTool.Test/UETest/UETests.cs
@@ -1,9 +1,4 @@
 using NUnit.Framework;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace AssetTool.Test.UETests
@@ -13,192 +8,49 @@ namespace AssetTool.Test.UETests
         [Test]
         public async Task Test_01_UE56_Assets()
         {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("UE56_Files.txt");
-            w.Start();
-            await Parallel.ForEachAsync(files, async (file, ct) =>
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                Assert.That(success, file);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+            await Test_UE_Files("UE56_Files");
         }
 
         [Test]
         public async Task Test_01_UE56_Maps()
         {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("UE56_Map_Files.txt");
-            w.Start();
-            foreach (var file in files)
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                Assert.That(success, $"[{files.ToList().IndexOf(file)}] {file}");
-            }
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+            await Test_UE_Files("UE56_Map_Files");
         }
 
         [Test]
         public async Task Test_02_UE55_Assets()
         {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("UE55_Files.txt");
-            w.Start();
-            await Parallel.ForEachAsync(files, async (file, ct) =>
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                Assert.That(success, file);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+            await Test_UE_Files("UE55_Files");
         }
 
         [Test]
         public async Task Test_02_UE55_Maps()
         {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("UE55_Map_Files.txt");
-            w.Start();
-            await Parallel.ForEachAsync(files, async (file, ct) =>
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                Assert.That(success, file);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+            await Test_UE_Files("UE55_Map_Files");
         }
 
         [Test]
         public async Task Test_03_UE54_Assets()
         {
-            ConcurrentBag<string> failedFiles = new();
-            ConcurrentBag<string> succeededFiles = new();
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("UE54_Files.txt");
-            w.Start();
-            await Parallel.ForEachAsync(files, async (file, ct) =>
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                UpdateFailedFiles(success, file, failedFiles, succeededFiles);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
-        }
-
-        [Test]
-        public async Task Test_04_UE53_Maps()
-        {
-            ConcurrentBag<string> failedFiles = new();
-            ConcurrentBag<string> succeededFiles = new();
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("UE53_Map_Files.txt");
-            w.Start();
-            await Parallel.ForEachAsync(files, async (file, ct) =>
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                UpdateFailedFiles(success, file, failedFiles, succeededFiles);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
-        }
-
-        [Test]
-        public async Task Test_04_UE53_Assets()
-        {
-            ConcurrentBag<string> failedFiles = new();
-            ConcurrentBag<string> succeededFiles = new();
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("UE53_Files.txt");
-            w.Start();
-            await Parallel.ForEachAsync(files, async (file, ct) =>
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                UpdateFailedFiles(success, file, failedFiles, succeededFiles);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+            await Test_UE_Files("UE54_Files");
         }
 
         [Test]
         public async Task Test_03_UE54_Maps()
         {
-            ConcurrentBag<string> failedFiles = new();
-            ConcurrentBag<string> succeededFiles = new();
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("UE54_Map_Files.txt");
-            w.Start();
-            await Parallel.ForEachAsync(files, async (file, ct) =>
-            {
-                bool success = await StructWriter.RebuildAssetFastAsync(file, "");
-                UpdateFailedFiles(success, file, failedFiles, succeededFiles);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
+            await Test_UE_Files("UE54_Map_Files");
         }
 
-        private static void UpdateLaunchSettingts(string file, bool success)
+        [Test]
+        public async Task Test_04_UE53_Assets()
         {
-            if (!success)
-            {
-                string path = "AssetTool\\Properties\\launchSettings.json";
-                string[] lines = File.ReadAllLines(path);
-                lines[5] = $"      \"commandLineArgs\": \"{file.Replace('\\', '/')} -log\"";
-                File.WriteAllLines(path, lines);
-            }
+            await Test_UE_Files("UE53_Files");
         }
 
-        private void UpdateFailedFiles(bool success, string file, ConcurrentBag<string> failedFiles, ConcurrentBag<string> succeededFiles)
+        [Test]
+        public async Task Test_04_UE53_Maps()
         {
-            if (!success)
-            {
-                failedFiles.Add(file);
-            }
-            else
-            {
-                succeededFiles.Add(file);
-            }
-        }
-
-        private void SaveFiles(string name, string[] allFiles, ConcurrentBag<string> failedFiles, ConcurrentBag<string> succeededFiles)
-        {
-            HashSet<string> failedHashset = failedFiles.ToHashSet();
-            failedFiles.Clear();
-
-            HashSet<string> succeededHashset = succeededFiles.ToHashSet();
-            succeededFiles.Clear();
-
-            List<string> succeededFilesSorted = [];
-            List<string> failedFilesSorted = [];
-
-            foreach (string file in allFiles)
-            {
-                if (failedHashset.Contains(file))
-                {
-                    failedFilesSorted.Add(file);
-                }
-                else if (succeededHashset.Contains(file))
-                {
-                    succeededFilesSorted.Add(file);
-                }
-            }
-
-            if (failedFilesSorted.Count > 0)
-            {
-                UpdateLaunchSettingts(failedFilesSorted.First(), false);
-            }
-
-            File.WriteAllLines($"{name}_Failed.txt", failedFilesSorted);
-            File.WriteAllLines($"{name}_Succeeded.txt", succeededFilesSorted);
+            await Test_UE_Files("UE53_Map_Files");
         }
     }
 }
