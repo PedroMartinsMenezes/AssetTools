ccd2bda0b9debe58893f6a8ca61cfb0b978bd924
pmmenezes <protopedro@gmail.com>
2025-09-15 00:09:11 -0300

    UE53_Files - 100%

--- AssetTool.Test/UETest/UETests.cs
+++ AssetTool.Test/UETest/UETests.cs
@@ -44,13 +44,13 @@ namespace AssetTool.Test.UETests
         [Test]
         public async Task Test_04_UE53_Assets()
         {
-            await Test_UE_Files("UE53_Files", true);
+            await Test_UE_Files("UE53_Files");
         }
 
         [Test]
         public async Task Test_04_UE53_Maps()
         {
-            await Test_UE_Files("UE53_Map_Files", true);
+            await Test_UE_Files("UE53_Map_Files");
         }
     }
 }
