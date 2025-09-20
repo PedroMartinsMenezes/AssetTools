6ce7ac90ef73dcfdd66cfa4f6b2e96064a91aa35
pmmenezes <protopedro@gmail.com>
2025-09-19 22:59:08 -0300

    UE427 - 100%

--- AssetTool/UE/Runtime/CoreUObject/Public/StructUtils/UserDefinedStruct.cs
+++ AssetTool/UE/Runtime/CoreUObject/Public/StructUtils/UserDefinedStruct.cs
@@ -3,13 +3,21 @@ namespace AssetTool
     [JsonAsset("UserDefinedStruct")]
     public class UUserDefinedStruct : UScriptStruct
     {
-        public Dictionary<string, object> UserTags = new();
+        public UScriptStruct UScriptStruct;
 
         [Location("void UUserDefinedStruct::Serialize(FStructuredArchive::FRecord Record)")]
         public override ITransferible Move(Transfer transfer)
         {
             base.Move(transfer);
-            transfer.MoveTags(UserTags, 0, this);
+            if (transfer.GlobalObjects.CurrentObject.ObjectFlags.HasFlag(EObjectFlags.RF_ClassDefaultObject))
+            {
+                return this;
+            }
+            if (transfer.Supports.UserDefinedStructsStoreDefaultInstance)
+            {
+                UScriptStruct ??= new UScriptStruct();
+                UScriptStruct.SerializeItem(transfer);
+            }
             return this;
         }
     }
