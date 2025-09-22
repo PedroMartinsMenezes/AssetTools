cf3dd537cce078d8baea36d377c1b269ee99d8cd
pmmenezes <protopedro@gmail.com>
2025-09-21 23:56:21 -0300

    UE427 Failed filed: 13

---
+++ AssetTool/UE/Runtime/Core/Public/Serialization/CustomVersion.cs
@@ -0,0 +1,38 @@
+﻿namespace AssetTool
+{
+    public class FEnumCustomVersion_DEPRECATED : ITransferible
+    {
+        public uint32 Tag;
+        public int32 Version;
+
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref Tag);
+            transfer.Move(ref Version);
+            return this;
+        }
+    }
+
+    public class FGuidCustomVersion_DEPRECATED : ITransferible
+    {
+        public FGuid Key;
+        public int32 Version;
+        public FString FriendlyName;
+
+        public ITransferible Move(Transfer transfer)
+        {
+            transfer.Move(ref Key);
+            transfer.Move(ref Version);
+            transfer.Move(ref FriendlyName);
+            return this;
+        }
+    }
+
+    public enum ECustomVersionSerializationFormat : uint8
+    {
+        Unknown,
+        Guids,
+        Enums,
+        Optimized,
+    }
+}
