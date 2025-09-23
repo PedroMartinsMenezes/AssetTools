51ac9808e3d7ea4b3801cff4cb6e073510c4659d
pmmenezes <protopedro@gmail.com>
2025-09-23 01:58:21 -0300

    UE427 Failed files: 10

--- AssetTool/UE/Plugins/2D/Paper2D/Source/Paper2D/Classes/PaperSprite.cs
+++ AssetTool/UE/Plugins/2D/Paper2D/Source/Paper2D/Classes/PaperSprite.cs
@@ -3,6 +3,12 @@ namespace AssetTool
     [JsonAsset("PaperSprite")]
     public class UPaperSprite : UObject
     {
+        public UPaperSprite()
+        {
+            ArrayMovers.Add("Vertices", (transfer, value) => value.ToObject<FVector2D>(transfer).Move(transfer));
+            ArrayMovers.Add("BakedRenderData", (transfer, value) => value.ToObject<FVector4>(transfer).Move(transfer));
+        }
+
         public override ITransferible Move(Transfer transfer)
         {
             return base.Move(transfer);
