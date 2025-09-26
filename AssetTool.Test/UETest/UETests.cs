6d699b9025a5c234d7361b6dfa1d73aea9f43d08
pmmenezes <protopedro@gmail.com>
2025-09-25 23:06:15 -0300

    UE426 Failed files: 2

--- AssetTool.Test/UETest/UETests.cs
+++ AssetTool.Test/UETest/UETests.cs
@@ -4,7 +4,7 @@ namespace AssetTool.Test.UETests
 {
     public class UETests : TestBase
     {
-        [Test] public void Test_UE426_Assets() => Test_UE_Files("UE426_Files");
+        [Test] public void Test_UE426_Assets() => Test_UE_Files("UE426_Files", true);
         [Test] public void Test_UE426_Maps() => Test_UE_Files("UE426_Map_Files");
         [Test] public void Test_UE427_Assets() => Test_UE_Files("UE427_Files");
         [Test] public void Test_UE427_Maps() => Test_UE_Files("UE427_Map_Files");
