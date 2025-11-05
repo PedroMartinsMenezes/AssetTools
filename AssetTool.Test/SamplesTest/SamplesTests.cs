b34cd25546c8970c2df956da8c7628934ad1388c
pmmenezes <protopedro@gmail.com>
2025-11-05 10:31:30 -0300

    Refactored: SamplesTests

--- AssetTool.Test/SamplesTest/SamplesTests.cs
+++ AssetTool.Test/SamplesTest/SamplesTests.cs
@@ -1,58 +1,13 @@
 using NUnit.Framework;
-using System.Diagnostics;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace AssetTool.Test.SamplesTest
 {
     public class SamplesTests : TestBase
     {
-        [Test]
-        public void Test_Cropout_Assets()
-        {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("AssetTool.Test\\InputFiles\\Cropout_Files.txt");
-            w.Start();
-            Parallel.ForEach(files, file =>
-            {
-                bool success = StructWriter.RebuildAssetFast(file, "");
-                Assert.That(success, file);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
-        }
+        [Test] public void Test_Cropout_Assets() => Test_UE_Files("Cropout_Files");
 
-        [Test]
-        public void Test_StackOBot_Assets()
-        {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("AssetTool.Test\\InputFiles\\StackOBot_Files.txt");
-            w.Start();
-            Parallel.ForEach(files, file =>
-            {
-                bool success = StructWriter.RebuildAssetFast(file, "");
-                Assert.That(success, file);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
-        }
+        [Test] public void Test_StackOBot_Assets() => Test_UE_Files("StackOBot_Files");
 
-        [Test]
-        public void Test_Lyra_Assets()
-        {
-            Stopwatch w = new Stopwatch();
-            var files = File.ReadAllLines("AssetTool.Test\\InputFiles\\Lyra_Files.txt");
-            w.Start();
-            Parallel.ForEach(files, file =>
-            {
-                bool success = StructWriter.RebuildAssetFast(file, "");
-                Assert.That(success, file);
-            });
-            w.Stop();
-            TestContext.WriteLine($"File Count   : {files.Length}");
-            TestContext.WriteLine($"Total Seconds: {w.Elapsed.TotalSeconds:0.00}");
-        }
+        [Test] public void Test_Lyra_Assets() => Test_UE_Files("Lyra_Files");
     }
 }
