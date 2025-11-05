faa966d9227d34ef81fa8ce2d6ea2909a1fc930d
pmmenezes <protopedro@gmail.com>
2025-11-05 10:28:54 -0300

    Fix UDebugDrawComponent

--- AssetTool/UE/Runtime/Engine/Classes/DebugFolder/DebugDrawComponent.cs
+++ AssetTool/UE/Runtime/Engine/Classes/DebugFolder/DebugDrawComponent.cs
@@ -1,34 +1,11 @@
 ﻿namespace AssetTool
 {
     [JsonAsset("DebugDrawComponent")]
-    public class UDebugDrawComponent : UPrimitiveComponent
+    public class UDebugDrawComponent : UEngineSubsystem
     {
-        public FSphere BoundingSphere;
-        public float MinDistance;
-        public float MaxDistance;
-        public float TexelFactor;
-
-        [Location("FArchive& operator<<( FArchive& Ar, FStreamableTextureInstance& TextureInstance )")]
         public override ITransferible Move(Transfer transfer)
         {
-            if (transfer.Supports.VER_UE4_STREAMABLE_TEXTURE_AABB)
-            {
-                transfer.Move(ref Bounds);
-            }
-            else
-            {
-                transfer.Move(ref BoundingSphere);
-            }
-
-            if (transfer.Supports.VER_UE4_STREAMABLE_TEXTURE_MIN_MAX_DISTANCE)
-            {
-                transfer.Move(ref MinDistance);
-                transfer.Move(ref MaxDistance);
-            }
-
-            transfer.Move(ref TexelFactor);
-
-            return this;
+            return base.Move(transfer);
         }
     }
 }
