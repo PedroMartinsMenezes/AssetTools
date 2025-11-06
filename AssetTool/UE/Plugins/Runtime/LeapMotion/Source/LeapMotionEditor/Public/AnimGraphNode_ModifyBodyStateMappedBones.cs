7adc246067353856866e83ab160a60ee4c871f7c
pmmenezes <protopedro@gmail.com>
2025-11-05 23:26:43 -0300

    UAnimGraphNode_ModifyBodyStateMappedBones

---
+++ AssetTool/UE/Plugins/Runtime/LeapMotion/Source/LeapMotionEditor/Public/AnimGraphNode_ModifyBodyStateMappedBones.cs
@@ -0,0 +1,11 @@
+﻿namespace AssetTool
+{
+    [JsonAsset("AnimGraphNode_ModifyBodyStateMappedBones")]
+    public class UAnimGraphNode_ModifyBodyStateMappedBones : UK2Node
+    {
+        public override ITransferible Move(Transfer transfer)
+        {
+            return base.Move(transfer);
+        }
+    }
+}
