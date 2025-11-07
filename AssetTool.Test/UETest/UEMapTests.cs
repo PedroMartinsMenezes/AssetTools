fe0a6d0a2fbc061f6650f3da4e545ddf507c3083
pmmenezes <protopedro@gmail.com>
2025-11-07 14:27:33 -0300

    Test_UE421_Maps - 100%

--- AssetTool.Test/UETest/UEMapTests.cs
+++ AssetTool.Test/UETest/UEMapTests.cs
@@ -5,7 +5,7 @@ namespace AssetTool.Test.UETests
     public class UE4MapTests : TestBase
     {
         [Test][Ignore("Broken")] public void Test_UE420_Maps() => Test_UE_Files("UE420_Map_Files");
-        [Test][Ignore("Broken")] public void Test_UE421_Maps() => Test_UE_Files("UE421_Map_Files");
+        [Test] public void Test_UE421_Maps() => Test_UE_Files("UE421_Map_Files");
         [Test] public void Test_UE422_Maps() => Test_UE_Files("UE422_Map_Files");
         [Test] public void Test_UE423_Maps() => Test_UE_Files("UE423_Map_Files");
         [Test] public void Test_UE424_Maps() => Test_UE_Files("UE424_Map_Files");
