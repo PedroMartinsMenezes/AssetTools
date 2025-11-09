fb3d482ac135f48b8b4355e41ac1e750259fb407
pmmenezes <protopedro@gmail.com>
2025-11-09 00:48:02 -0300

    Test_UE411_Assets - 100%

--- AssetTool/UE/Runtime/Engine/Public/Materials/MaterialExpressionDynamicParameter.cs
+++ AssetTool/UE/Runtime/Engine/Public/Materials/MaterialExpressionDynamicParameter.cs
@@ -3,6 +3,11 @@ namespace AssetTool
     [JsonAsset("MaterialExpressionDynamicParameter")]
     public class UMaterialExpressionDynamicParameter : UMaterialExpression
     {
+        public UMaterialExpressionDynamicParameter()
+        {
+            ArrayMovers.Add("ParamNames", (transfer, value) => value.ToObject<FString>(transfer).Move(transfer));
+        }
+
         public override ITransferible Move(Transfer transfer)
         {
             return base.Move(transfer);
--- AssetTool/UE/Runtime/Engine/Public/Materials/MaterialFunction.cs
+++ AssetTool/UE/Runtime/Engine/Public/Materials/MaterialFunction.cs
@@ -12,6 +12,13 @@ namespace AssetTool
     [JsonAsset("MaterialFunction")]
     public class UMaterialFunction : UMaterialFunctionInterface
     {
+        public UMaterialFunction()
+        {
+            ArrayMovers.Add("FunctionExpressions", (transfer, value) => value.ToObject<TUInt32>(transfer).Move(transfer));
+            ArrayMovers.Add("FunctionEditorComments", (transfer, value) => value.ToObject<TUInt32>(transfer).Move(transfer));
+            ArrayMovers.Add("LibraryCategories", (transfer, value) => value.ToObject<FString>(transfer).Move(transfer));
+        }
+
         public override ITransferible Move(Transfer transfer)
         {
             return base.Move(transfer);
