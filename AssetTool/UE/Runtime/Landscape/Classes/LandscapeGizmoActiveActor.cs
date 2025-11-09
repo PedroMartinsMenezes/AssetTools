485a2fed05b0e7f247226848bb8110be20fee517
pmmenezes <protopedro@gmail.com>
2025-11-09 00:11:35 -0300

    Test_UE412_Maps - 100%

--- AssetTool/UE/Runtime/Landscape/Classes/LandscapeGizmoActiveActor.cs
+++ AssetTool/UE/Runtime/Landscape/Classes/LandscapeGizmoActiveActor.cs
@@ -3,6 +3,11 @@ namespace AssetTool
     [JsonAsset("LandscapeGizmoActiveActor")]
     public class ALandscapeGizmoActiveActor : ALandscapeGizmoActor
     {
+        public ALandscapeGizmoActiveActor()
+        {
+            ArrayMovers.Add("SampledHeight", (transfer, value) => value.ToObject<FVector>(transfer).Move(transfer));
+        }
+
         public override ITransferible Move(Transfer transfer)
         {
             return base.Move(transfer);
