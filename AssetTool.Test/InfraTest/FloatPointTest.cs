3fb4203cd9fc9b302bda35cecc20034bf65a5f8e
pmmenezes <protopedro@gmail.com>
2026-06-07 15:17:29 -0300

    Test

---
+++ AssetTool.Test/InfraTest/FloatPointTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace AssetTool.Test.InfraTest
+{
+    public class FloatPointTest : TestBase
+    {
+        [Test]
+        public void Double_Serialization_Should_Fail_When_Default()
+        {
+            double original = 2.9802322387695313E-08;
+
+            string text = original.ToString();
+
+            double reconstructed = double.Parse(text);
+
+            Assert.That(reconstructed, Is.Not.EqualTo(original));
+        }
+
+        [Test]
+        public void Double_Serialization_Should_Succeeded_When_G17()
+        {
+            double original = 2.9802322387695313E-08;
+
+            string text = original.ToString("G17");
+
+            double reconstructed = double.Parse(text);
+
+            Assert.That(reconstructed, Is.EqualTo(original));
+        }
+    }
+}
