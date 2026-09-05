cef85c8574ad491fd9d6c11071ab134789c32e25
pmmenezes <protopedro@gmail.com>
2026-09-05 19:05:00 -0300

    Separate tests

--- AssetTool.Test/UETest/UE5Tests.cs
+++ AssetTool.Test/UETest/UE5Tests.cs
@@ -2,25 +2,60 @@ using NUnit.Framework;
 
 namespace AssetTool.Test.UE5Tests
 {
-    public class UE5AssetTests : TestBase
+    public class UE50AssetTests : TestBase
     {
         [Test] public void Test_UE50_Assets() => Test_UE_Files("UE50_Files");
+    }
+    public class UE51AssetTests : TestBase
+    {
         [Test] public void Test_UE51_Assets() => Test_UE_Files("UE51_Files");
+    }
+    public class UE52AssetTests : TestBase
+    {
         [Test] public void Test_UE52_Assets() => Test_UE_Files("UE52_Files");
+    }
+    public class UE53AssetTests : TestBase
+    {
         [Test] public void Test_UE53_Assets() => Test_UE_Files("UE53_Files");
+    }
+    public class UE54AssetTests : TestBase
+    {
         [Test] public void Test_UE54_Assets() => Test_UE_Files("UE54_Files");
+    }
+    public class UE55AssetTests : TestBase
+    {
         [Test] public void Test_UE55_Assets() => Test_UE_Files("UE55_Files");
+    }
+    public class UE56AssetTests : TestBase
+    {
         [Test] public void Test_UE56_Assets() => Test_UE_Files("UE56_Files");
     }
-
-    public class UE5MapTests : TestBase
+    public class UE50MapTests : TestBase
     {
         [Test] public void Test_UE50_Maps() => Test_UE_Files("UE50_Map_Files");
+    }
+    public class UE51MapTests : TestBase
+    {
         [Test] public void Test_UE51_Maps() => Test_UE_Files("UE51_Map_Files");
+    }
+    public class UE52MapTests : TestBase
+    {
         [Test] public void Test_UE52_Maps() => Test_UE_Files("UE52_Map_Files");
+    }
+    public class UE53MapTests : TestBase
+    {
         [Test] public void Test_UE53_Maps() => Test_UE_Files("UE53_Map_Files");
+    }
+    public class UE54MapTests : TestBase
+    {
         [Test] public void Test_UE54_Maps() => Test_UE_Files("UE54_Map_Files");
+    }
+    public class UE55MapTests : TestBase
+    {
         [Test] public void Test_UE55_Maps() => Test_UE_Files("UE55_Map_Files");
+    }
+    public class UE56MapTests : TestBase
+    {
         [Test] public void Test_UE56_Maps() => Test_UE_Files("UE56_Map_Files");
     }
 }
