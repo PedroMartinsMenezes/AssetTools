3a336a069841e86b3f8fc2661c0465d7e9f977c3
pmmenezes <protopedro@gmail.com>
2026-09-19 14:53:41 -0300

    Remove

--- AssetTool/UE/Runtime/CoreUObject/Public/UObject/UnrealType/FPropertySerializerBase.cs
+++ AssetTool/UE/Runtime/CoreUObject/Public/UObject/UnrealType/FPropertySerializerBase.cs
@@ -5,89 +5,7 @@ namespace AssetTool
 {
     public abstract class FPropertySerializerBase<T> where T : FProperty, new()
     {
-        protected T ReadBaseProperties(JsonElement root)
-        {
-            T value = new()
-            {
-                ArrayDim = 1,
-                PropertyFlags = EPropertyFlags.CPF_None,
-                RepIndex = 0,
-                RepNotifyFunc = new FName("None"),
-                BlueprintReplicationCondition = 0,
-                FlagsPrivate = EObjectFlags.RF_Public,
-                HasMetaData = false
-            };
-
-            if (root.TryGetProperty("ArrayDim", out var arrayDim))
-                value.ArrayDim = arrayDim.GetInt32();
-
-            if (root.TryGetProperty("PropertyFlags", out var propertyFlags))
-                Enum.TryParse(propertyFlags.GetString(), out value.PropertyFlags);
-
-            if (root.TryGetProperty("RepIndex", out var repIndex))
-                value.RepIndex = repIndex.GetUInt16();
-
-            if (root.TryGetProperty("RepNotifyFunc", out var repNotifyFunc))
-                value.RepNotifyFunc = new FName(repNotifyFunc.GetString());
-
-            if (root.TryGetProperty("BlueprintReplicationCondition", out var condition))
-                value.BlueprintReplicationCondition = condition.GetByte();
-
-            if (root.TryGetProperty("NamePrivate", out var namePrivate))
-                value.NamePrivate = new FName(namePrivate.GetString());
-
-            if (root.TryGetProperty("FlagsPrivate", out var flagsPrivate))
-                Enum.TryParse(flagsPrivate.GetString(), out value.FlagsPrivate);
-
-            if (root.TryGetProperty("MetaDataMap", out var metaDataMap))
-            {
-                value.HasMetaData = true;
-                foreach (var property in metaDataMap.EnumerateObject())
-                {
-                    value.MetaDataMap.Add(new FName(property.Name), new FString(property.Value.GetString() ?? string.Empty));
-                }
-            }
-
-            return value;
-        }
-
-        protected static void WriteBaseProperties(Utf8JsonWriter writer, T value)
-        {
-            writer.WriteString("__type", value.GetType().Name);
-
-            writer.WriteString("NamePrivate", value.NamePrivate.ToString());
-
-            if (value.PropertyFlags != EPropertyFlags.CPF_None)
-                writer.WriteString("PropertyFlags", value.PropertyFlags.ToString());
-
-            if (value.FlagsPrivate != EObjectFlags.RF_Public)
-                writer.WriteString("FlagsPrivate", value.FlagsPrivate.ToString());
-
-            if (value.ArrayDim != 1)
-                writer.WriteNumber("ArrayDim", value.ArrayDim);
-
-            if (value.RepIndex != 0)
-                writer.WriteNumber("RepIndex", value.RepIndex);
-
-            if (value.RepNotifyFunc.Value != "None")
-                writer.WriteString("RepNotifyFunc", value.RepNotifyFunc.ToString());
-
-            if (value.BlueprintReplicationCondition != 0)
-                writer.WriteNumber("BlueprintReplicationCondition", value.BlueprintReplicationCondition);
-
-            if (value.HasMetaData)
-            {
-                writer.WritePropertyName("MetaDataMap");
-                writer.WriteStartObject();
-                foreach (var kvp in value.MetaDataMap)
-                {
-                    writer.WriteString(kvp.Key.ToString(), kvp.Value.ToString());
-                }
-                writer.WriteEndObject();
-            }
-        }
-
-        protected T ReadKeyValue(string key, JsonElement root)
+        protected static T ReadKeyValue(string key, JsonElement root)
         {
             T value = new();
 
@@ -115,7 +33,7 @@ namespace AssetTool
             return value;
         }
 
-        protected void WriteKeyValue(Utf8JsonWriter writer, JsonSerializerOptions options, T value, string name, Dictionary<string, object> inlineFields = null, Dictionary<string, object> fields = null)
+        protected static void WriteKeyValue(Utf8JsonWriter writer, JsonSerializerOptions options, T value, string name, Dictionary<string, object> inlineFields = null, Dictionary<string, object> fields = null)
         {
             StringBuilder builder = new();
 
