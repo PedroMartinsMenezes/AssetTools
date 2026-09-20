ed9554fc6d8ce71005a4594110b6c749bf52a194
pmmenezes <protopedro@gmail.com>
2026-09-20 16:41:18 -0300

    Sequential tests

---
+++ AssetTool.Test/UETestSequential/UE_410_419_Tests.cs
@@ -0,0 +1,86 @@
+using NUnit.Framework;
+
+namespace AssetTool.Test.Sequential
+{
+    public class UE410AssetTests : TestBase
+    {
+        [Test] public void Test_UE410_Assets() => Test_UE_Files_Sequential("UE410_Files");
+    }
+    public class UE411AssetTests : TestBase
+    {
+        [Test] public void Test_UE411_Assets() => Test_UE_Files_Sequential("UE411_Files");
+    }
+    public class UE412AssetTests : TestBase
+    {
+        [Test] public void Test_UE412_Assets() => Test_UE_Files_Sequential("UE412_Files");
+    }
+    public class UE413AssetTests : TestBase
+    {
+        [Test] public void Test_UE413_Assets() => Test_UE_Files_Sequential("UE413_Files");
+    }
+    public class UE414AssetTests : TestBase
+    {
+        [Test] public void Test_UE414_Assets() => Test_UE_Files_Sequential("UE414_Files");
+    }
+    public class UE415AssetTests : TestBase
+    {
+        [Test] public void Test_UE415_Assets() => Test_UE_Files_Sequential("UE415_Files");
+    }
+    public class UE416AssetTests : TestBase
+    {
+        [Test] public void Test_UE416_Assets() => Test_UE_Files_Sequential("UE416_Files");
+    }
+    public class UE417AssetTests : TestBase
+    {
+        [Test] public void Test_UE417_Assets() => Test_UE_Files_Sequential("UE417_Files");
+    }
+    public class UE418AssetTests : TestBase
+    {
+        [Test] public void Test_UE418_Assets() => Test_UE_Files_Sequential("UE418_Files");
+    }
+    public class UE419AssetTests : TestBase
+    {
+        [Test] public void Test_UE419_Assets() => Test_UE_Files_Sequential("UE419_Files");
+    }
+
+    public class UE410MapTests : TestBase
+    {
+        [Test] public void Test_UE410_Maps() => Test_UE_Files_Sequential("UE410_Map_Files");
+    }
+    public class UE411MapTests : TestBase
+    {
+        [Test] public void Test_UE411_Maps() => Test_UE_Files_Sequential("UE411_Map_Files");
+    }
+    public class UE412MapTests : TestBase
+    {
+        [Test] public void Test_UE412_Maps() => Test_UE_Files_Sequential("UE412_Map_Files");
+    }
+    public class UE413MapTests : TestBase
+    {
+        [Test] public void Test_UE413_Maps() => Test_UE_Files_Sequential("UE413_Map_Files");
+    }
+    public class UE414MapTests : TestBase
+    {
+        [Test] public void Test_UE414_Maps() => Test_UE_Files_Sequential("UE414_Map_Files");
+    }
+    public class UE415MapTests : TestBase
+    {
+        [Test] public void Test_UE415_Maps() => Test_UE_Files_Sequential("UE415_Map_Files");
+    }
+    public class UE416MapTests : TestBase
+    {
+        [Test] public void Test_UE416_Maps() => Test_UE_Files_Sequential("UE416_Map_Files");
+    }
+    public class UE417MapTests : TestBase
+    {
+        [Test] public void Test_UE417_Maps() => Test_UE_Files_Sequential("UE417_Map_Files");
+    }
+    public class UE418MapTests : TestBase
+    {
+        [Test] public void Test_UE418_Maps() => Test_UE_Files_Sequential("UE418_Map_Files");
+    }
+    public class UE419MapTests : TestBase
+    {
+        [Test] public void Test_UE419_Maps() => Test_UE_Files_Sequential("UE419_Map_Files");
+    }
+}
